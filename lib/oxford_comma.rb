fa434f0f933b7ffa00c528b3f67b2026d46a7b08
Ingrid Wong <yw340@georgetown.edu>
2018-01-12 21:29:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,3 +14,16 @@ def oxford_comma(array)
     output<<"and #{array[array.length-1]}"
   end
 end
+
+
+def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    array.insert(-1, "and")
+    array.join(", ")
+  end
+
+end 
